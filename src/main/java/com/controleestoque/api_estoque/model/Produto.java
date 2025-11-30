f66adc61faa6d11d4429abb8d53d47ef10134941
ArthurDombroski <tutudombroski@gmail.com>
2025-11-30 20:48:34 -0300

    feat: model produto feito

--- src/main/java/com/controleestoque/api_estoque/model/Produto.java
+++ src/main/java/com/controleestoque/api_estoque/model/Produto.java
@@ -1,5 +1,95 @@
 package com.controleestoque.api_estoque.model;
 
+import java.math.BigDecimal; // import para um tipo de dado no BD
+import java.util.Set;
+
+import jakarta.persistence.*; // import para anotações JPA
+
+@Entity // Define a classe como uma entidade JPA
+@Table(name = "tb_produtos") // Mapeia a entidade para a tabela "tb_produtos"
+
 public class Produto {
     
+    @Id // Define o campo como chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Define id como auto incremento
+    private Long id; // id = auto incremento = chave primária
+
+    private String nome;
+
+    private BigDecimal preco;
+
+
+    //Relacionamento com Estoque
+    @OneToOne(mappedBy = "produto", cascade = CascadeType.ALL, orphanRemoval = true) //orphanRemoval apaga automaticamente do banco os filhos que forem removidos da coleção do pai.
+    private Estoque estoque;
+
+    //Relacionamento com Categoria
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+    //Relacionamento com fornecedores
+    @ManyToMany
+    @JoinTable(
+        name = "tb_produto_fornecedor",
+        joinColumns = @JoinColumn(name = "produto_id"),
+        inverseJoinColumns = @JoinColumn(name = "fornecedor_id")
+    )
+    private Set<Fornecedor> fornecedores;
+
+    public Produto (){}
+
+    public Produto(String nome, BigDecimal preco, Estoque estoque, Categoria categoria,
+        Set<Fornecedor> fornecedores) {
+            this.nome = nome;
+            this.preco = preco;
+            this.estoque = estoque;
+            this.categoria = categoria;
+            this.fornecedores = fornecedores;
+        }
+
+        public Long getId() {
+            return id;
+        }
+        public void SetId(Long id) {
+            this.id = id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+        public void setNome() {
+            this.nome = nome;
+        }
+
+        public BigDecimal getPreco() {
+            return preco;
+        }
+        public void setPreco(BigDecimal preco) {
+            this.preco = preco;
+        }
+
+        public Estoque getEstoque() {
+            return estoque;
+        }
+        public void setEstoque(Estoque estoque) {
+            this.estoque = estoque;
+        }
+
+        public Categoria getCategoria() {
+            return categoria;
+        }
+        public void setCategoria(Categoria categoria) {
+            this.categoria = categoria;
+        }
+
+        public Set<Fornecedor> getFornecedores() {
+            return fornecedores;
+        }
+        public void setFornecedores(Set<Fornecedor> fornecedores) {
+            this.fornecedores = fornecedores;
+        }
+
+
+
 }
