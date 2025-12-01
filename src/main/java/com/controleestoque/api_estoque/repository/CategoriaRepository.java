840354140764b9a74a73e45d29665786335d9d51
ArthurDombroski <tutudombroski@gmail.com>
2025-11-30 22:16:01 -0300

    feat: controller e repository Categoria feitos

---
+++ src/main/java/com/controleestoque/api_estoque/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.controleestoque.api_estoque.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.controleestoque.api_estoque.model.Categoria;
+import com.controleestoque.api_estoque.model.Produto;
+
+import java.util.List;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    List<Categoria> findByNome (String nome);
+    List<Categoria> findByNomeContaining (String parteDoNome);
+}
